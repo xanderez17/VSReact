1f766b948bef9a5898fa0696670404c5c0e1814a
Kevin Hernandez <kevxanderez17@gmail.com>
2022-03-21 20:20:46 -0500

    Subida 1 REACT

---
+++ src/services/ProductService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export class ProductService {
+
+    //baseUrl = "http://localhost:8080/api/products/";
+     baseUrl = "https://rest-api-mongo-m5a.herokuapp.com/api/products/";
+
+    create(product){
+        return axios.post(this.baseUrl+"product/", product).then(res => res.data);
+    }
+
+    readAll(){
+        return axios.get(this.baseUrl).then(res => res.data);
+    }
+
+    update(product){
+        return axios.put(this.baseUrl+"product/"+product._id, product).then(res => res.data);
+    }
+
+    delete(id){
+        return axios.delete(this.baseUrl+"product/"+id).then(res => res.data);
+    }
+}
